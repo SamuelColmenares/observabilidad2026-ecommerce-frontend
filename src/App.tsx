0c84bc97439eceb2b6ad74eb7bbeec1a00c731c0
Juan Carlos De Jesus <juan.de.jesus@accenture.com>
2026-05-09 00:00:35 -0500

    agrega libreria logs para enviar a grafana

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,45 @@ import { fetchProducts } from "./services/products-services";
 import type { Product } from "./product/Product";
 import { Link, Outlet } from "react-router";
 import { ProductsContext } from "./context/ProductsContext";
+import { SeverityNumber, logs } from '@opentelemetry/api-logs';
+import {
+  LoggerProvider,
+  SimpleLogRecordProcessor,
+  ConsoleLogRecordExporter,
+  BatchLogRecordProcessor
+} from '@opentelemetry/sdk-logs'
+import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http';
+import {
+  BatchSpanProcessor,
+  WebTracerProvider,
+} from '@opentelemetry/sdk-trace-web';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+const collectorOptions = {
+  url: 'http://172.26.190.162:3005/v1/logs', // url is optional and can be omitted - default is http://localhost:4318/v1/traces
+  headers: {}, // an optional object containing custom headers to be sent with each request
+  concurrencyLimit: 10, // an optional limit on pending requests
+};
+const logExporter = new OTLPLogExporter(collectorOptions);
+const loggerProvider = new LoggerProvider({
+  processors: [new BatchLogRecordProcessor(logExporter)]
+});
+
+const logger = loggerProvider.getLogger('default');
 
 export const App: React.FC = () => {
   const [products, setProducts] = React.useState<Product[]>([]);
   const [search, setSearch] = React.useState<string | undefined>(undefined);
   const [loading, setLoading] = React.useState(false);
   const [isSearchDirty, setIsSearchDirty] = React.useState(false);
+
+  useEffect(() => {
+    logger.emit({
+      severityNumber: SeverityNumber.INFO,
+      severityText: 'info',
+      body: 'this is a log body',
+      attributes: { 'log.type': 'custom' },
+    });
+  })
 
   const onSearchEvent = async () => {
     setIsSearchDirty(true);
